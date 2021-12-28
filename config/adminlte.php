96990ebece07071c9db6ed434a7542af0c8d37fa
abrahamrendy <aeroponse@gmail.com>
2021-12-28 17:34:25 +0700

    set up excel export

--- config/adminlte.php
+++ config/adminlte.php
@@ -67,9 +67,9 @@ return [
     'usermenu_enabled' => true,
     'usermenu_header' => true,
     'usermenu_header_class' => 'bg-primary',
-    'usermenu_image' => true,
-    'usermenu_desc' => true,
-    'usermenu_profile_url' => true,
+    'usermenu_image' => false,
+    'usermenu_desc' => false,
+    'usermenu_profile_url' => false,
     /*
     |--------------------------------------------------------------------------
     | Layout
@@ -223,99 +223,117 @@ return [
     |
     */
 
-    'menu' => [
-        // Navbar items:
-        [
-            'type'         => 'navbar-search',
-            'text'         => 'search',
-            'topnav_right' => true,
-        ],
-        [
-            'type'         => 'fullscreen-widget',
-            'topnav_right' => true,
-        ],
+    // 'menu' => [
+    //     // Navbar items:
+    //     [
+    //         'type'         => 'navbar-search',
+    //         'text'         => 'search',
+    //         'topnav_right' => true,
+    //     ],
+    //     [
+    //         'type'         => 'fullscreen-widget',
+    //         'topnav_right' => true,
+    //     ],
 
-        // Sidebar items:
+    //     // Sidebar items:
+    //     [
+    //         'type' => 'sidebar-menu-search',
+    //         'text' => 'search',
+    //     ],
+    //     [
+    //         'text' => 'blog',
+    //         'url'  => 'admin/blog',
+    //         'can'  => 'manage-blog',
+    //     ],
+    //     [
+    //         'text'        => 'pages',
+    //         'url'         => 'admin/pages',
+    //         'icon'        => 'far fa-fw fa-file',
+    //         'label'       => 4,
+    //         'label_color' => 'success',
+    //     ],
+    //     ['header' => 'account_settings'],
+    //     [
+    //         'text' => 'profile',
+    //         'url'  => 'admin/settings',
+    //         'icon' => 'fas fa-fw fa-user',
+    //     ],
+    //     [
+    //         'text' => 'change_password',
+    //         'url'  => 'admin/settings',
+    //         'icon' => 'fas fa-fw fa-lock',
+    //     ],
+    //     [
+    //         'text'    => 'multilevel',
+    //         'icon'    => 'fas fa-fw fa-share',
+    //         'submenu' => [
+    //             [
+    //                 'text' => 'level_one',
+    //                 'url'  => '#',
+    //             ],
+    //             [
+    //                 'text'    => 'level_one',
+    //                 'url'     => '#',
+    //                 'submenu' => [
+    //                     [
+    //                         'text' => 'level_two',
+    //                         'url'  => '#',
+    //                     ],
+    //                     [
+    //                         'text'    => 'level_two',
+    //                         'url'     => '#',
+    //                         'submenu' => [
+    //                             [
+    //                                 'text' => 'level_three',
+    //                                 'url'  => '#',
+    //                             ],
+    //                             [
+    //                                 'text' => 'level_three',
+    //                                 'url'  => '#',
+    //                             ],
+    //                         ],
+    //                     ],
+    //                 ],
+    //             ],
+    //             [
+    //                 'text' => 'level_one',
+    //                 'url'  => '#',
+    //             ],
+    //         ],
+    //     ],
+    //     ['header' => 'labels'],
+    //     [
+    //         'text'       => 'important',
+    //         'icon_color' => 'red',
+    //         'url'        => '#',
+    //     ],
+    //     [
+    //         'text'       => 'warning',
+    //         'icon_color' => 'yellow',
+    //         'url'        => '#',
+    //     ],
+    //     [
+    //         'text'       => 'information',
+    //         'icon_color' => 'cyan',
+    //         'url'        => '#',
+    //     ],
+    // ],
+    'menu' => [
         [
-            'type' => 'sidebar-menu-search',
-            'text' => 'search',
-        ],
-        [
-            'text' => 'blog',
-            'url'  => 'admin/blog',
-            'can'  => 'manage-blog',
-        ],
-        [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'far fa-fw fa-file',
-            'label'       => 4,
-            'label_color' => 'success',
+            'text' => 'Dashboard',
+            'url'  => '/home',
+            'icon' => 'fas fa-fw fa-home',
         ],
         ['header' => 'account_settings'],
         [
             'text' => 'profile',
-            'url'  => 'admin/settings',
+            'url'  => 'profile',
             'icon' => 'fas fa-fw fa-user',
         ],
         [
             'text' => 'change_password',
-            'url'  => 'admin/settings',
+            'url'  => 'change-password',
             'icon' => 'fas fa-fw fa-lock',
-        ],
-        [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
         ],
     ],
 
