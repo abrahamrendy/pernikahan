d0c7f3db3c14369942e4c27c1ed6673fa97b81e1
abrahamrendy <aeroponse@gmail.com>
2022-02-18 16:38:14 +0700

    Update akte.blade.php

---
+++ resources/views/akte.blade.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hi</title>
+    <link href="{{ URL::asset('css/bootstrap.min.css'); }} " rel="stylesheet">
+    <!-- <link href="{{ public_path('css/bootstrap.min.css'); }} " rel="stylesheet"> -->
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+                <div class="col-md-12">
+                    <img>IMAGE
+                    <div class="float-end">No</div>
+                </div>
+                <div class="col-md-12 font-roboto-serif row">
+                    <h1 class="font-bold">GEREJA BETHEL INDONESIA</h1>
+                    <h6>Badan Hukum Gereja: SK Dirjen Bimas Kristen Protestan Departemen Agama R.I No. 41 Tahun 1972 dan SK Dirjen Bimas (Kristen) Protestan Departemen Agama R.I. No. 211 Tahun 1989 Tgl. 25 Nopember 1989</h6>
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4">
+                        <h4 class="font-semibold" style="text-decoration: underline; margin-bottom: 0px;">AKTA NIKAH</h4>
+                        <h6 class="font-semibold">No. 3165/B/SP/X/21</h6>
+                    </div>
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4 font-medium">
+                        <p class="font-size-small">
+                            "Demikianlah mereka bukan lagi dua melainkan satu, karena itu apa yang telah dipersatukan Allah, tidak boleh diceraikan manusia"
+                            <br>
+                            (Matius 19:6)
+                        </p>
+                    </div>
+                    <div class="col-md-4"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p>
+                        PADA HARI .... TANGGAL .... DIHADAPAN SIDANG JEMAAT TUHAN TELAH .... PERNIKAHAN YANG KUDUS DARI
+                    </p>
+                </div>
+
+                <div class="col-md-12">
+                    <h4>ASD ASD ASD</h4>
+                    <div class="row">
+                        <div class="col-md-6">
+                            dilahirkan di ...
+                        </div>
+                        <div class="col-md-6">
+                            tanggal ...
+                        </div>
+                        <div class="col-md-6">
+                            anak laki-laki dari ...
+                        </div>
+                        <div class="col-md-6">
+                            dan ...
+                        </div>
+                    </div>
+                    <p>dengan</p>
+                </div>
+
+                <div class="col-md-12">
+                    <h4>ASD ASD ASD</h4>
+                    <div class="row">
+                        <div class="col-md-6">
+                            dilahirkan di ...
+                        </div>
+                        <div class="col-md-6">
+                            tanggal ...
+                        </div>
+                        <div class="col-md-6">
+                            anak perempuan dari ...
+                        </div>
+                        <div class="col-md-6">
+                            dan ...
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-12">
+                    <p>
+                        Upacara Pernikahan yang kudus ini telah dilakukan dalam Nama TUHAN YESUS KRISTUS oleh: ...
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    IMG
+                </div>
+                <div class="col-md-6">
+                    <p>Bandung, ....</p>
+                    <p>GEREJA BETHEL INDONESIA</p>
+                    <p>Jl. Aruna No.19-Bandung 40174</p>
+                    <p>Pdt. DR. Ir. NIKO NJOTORAHARDJO</p>
+                    <p>GEMBALA SIDANG</p>
+                </div>
+            </div>
+        </div>
+</body>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto+Serif:wght@300;400;500;600;700&display=swap');
+
+    body {
+        text-align: center;
+    }
+
+    .font-roboto-serif {
+        font-family: 'Roboto Serif', sans-serif !important;
+    }
+
+    .font-semibold {
+        font-weight: 600 !important;
+    }
+
+    .font-bold {
+        font-weight: 700 !important;
+    }
+
+    .font-medium {
+        font-weight: 500 !important;
+    }
+
+    .font-size-small {
+        font-size: 9pt;
+    }
+</style>
+</html>
